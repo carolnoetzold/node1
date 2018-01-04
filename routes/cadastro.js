daa687dc546b24d965107ea6ccae5a0a79c547fe
carolnoetzold <caroline.tads@gmail.com>
2018-01-04 09:37:53 -0300

    Final

--- routes/cadastro.js
+++
@@ -1,9 +0,0 @@
-// var express = require('express');
-// var router = express.Router();
-
-// /* GET home page. */
-// router.get('/cadastros/cadastro', function(req, res, next) {
-//     res.sendFile(path.join(__dirname + '/views/cadastro.html'));
-// });
-
-// module.exports = router;
